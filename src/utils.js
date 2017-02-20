4494e825b5e4fcade0b0a767410faf464d2a1c40
Chmichev Boris <boris362@yandex.ru>
2017-02-20 23:19:05 +0300

    Github pages

--- src/utils.js
+++
@@ -1,7 +0,0 @@
-export const chanceOfRain = (pressure, temperature, amount) => {
-  var score = Math.log(amount + 1) * Math.log(pressure - 929) * Math.log(temperature - 9)
-  var mean = Math.min(Math.max(score, 0), 100)
-  var upperBound = Math.min(1.5 * mean, 100)
-  var lowerBound = Math.max(0.5 * mean, 0)
-  return { lowerBound, mean, upperBound }
-}
